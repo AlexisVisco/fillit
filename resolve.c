0f92281308a0d0b156221629f5fd70791e0b2a19
Cyrille Epalle <cepalle@z4r5p6.le-101.fr>
2017-11-14 19:07:42 +0100

    resolve_bit start and abort

--- resolve.c
+++ resolve.c
@@ -6,75 +6,26 @@
 /*   By: cepalle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 21:25:47 by cepalle           #+#    #+#             */
-/*   Updated: 2017/11/14 09:04:57 by cepalle          ###   ########.fr       */
+/*   Updated: 2017/11/14 18:56:17 by cepalle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+#include <stdio.h>
 
-/*
-static char	can_pose(t_memtet *memtet, short p, short x, short y)
-{
-	short	k;
-	short	xi;
-	short	yi;
-
-	k = 0;
-	while (k < 4)
-	{
-		xi = x + memtet->ltet[p].crds[k].x;
-		yi = y + memtet->ltet[p].crds[k].y;
-		if (xi >= memtet->c || yi >= memtet->c)
-			return (0);
-		if (inter_ltet(memtet, p, xi, yi))
-			return (0);
-		k++;
-	}
-	return (1);
-}
-
-static char	resolve_aux(t_memtet *memtet, short p)
-{
-	short	xi;
-	short	yi;
-
-	yi = 0;
-	while (yi < memtet->c)
-	{
-		xi = 0;
-		while (xi < memtet->c)
-		{
-			if (can_pose(memtet, p, xi, yi))
-			{
-				memtet->lcrds[p].x = xi;
-				memtet->lcrds[p].y = yi;
-				if (p + 1 == memtet->l)
-					return (1);
-				else if (resolve_aux(memtet, p + 1))
-					return (1);
-			}
-			xi++;
-		}
-		yi++;
-	}
-	return (0);
-}
-*/
 void		resolve(t_memtet *memtet)
 {
-	/*
-	memtet->c = 2;
-	while (memtet->c * memtet->c < 4 * memtet->l)
-		memtet->c++;
-	while (!resolve_aux(memtet, 0))
-		memtet->c++;
-	*/
-	size_t i;
+	short			i;
+	t_memtet_bit	memtet_bit;
 
 	i = 0;
-	t_memtet_bit memtet_bit;
 	memtet_to_bit(&memtet_bit, memtet);
-	while (i < 26)
+	printf("1\n");
+	resolve_bit(&memtet_bit);
+	printf("2\n");
+	memtet->c = memtet_bit.c;
+	printf("3\n");
+	while (i < memtet->l)
 	{
 		memtet->lcrds[i].x = memtet_bit.lcrds[i].x;
 		memtet->lcrds[i].y = memtet_bit.lcrds[i].y;
--- resolve_bit.c
+++ resolve_bit.c
@@ -6,20 +6,69 @@
 /*   By: cepalle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 08:37:15 by cepalle           #+#    #+#             */
-/*   Updated: 2017/11/14 09:11:53 by cepalle          ###   ########.fr       */
+/*   Updated: 2017/11/14 19:05:26 by cepalle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+#include "libft/libft.h"
+
+static void	toggle_tet(uint64_t mask, uint16_t *map, short x, short y)
+{
+	*((uint64_t *)(map + y)) ^= (mask << x);
+}
+
+static uint64_t	chek_map(uint64_t mask, uint16_t *map, short x, short y)
+{
+	return ((*(uint64_t *)(map + y)) & (mask << x));
+}
+
+static char	resolve_bit_aux(t_memtet_bit *memtet, uint16_t *map, short p)
+{
+	short	xi;
+	short	yi;
+	t_tetriminos_bit tet;
+
+	tet = memtet->ltet[p];
+	yi = 0;
+	while (yi + tet.l < memtet->c)
+	{
+		xi = 0;
+		while (xi + tet.h < memtet->c)
+		{
+			if (chek_map(tet.mask, map, xi, yi))
+			{
+				memtet->lcrds[p].x = xi;
+				memtet->lcrds[p].y = yi;
+				toggle_tet(tet.mask, map, xi, yi);
+				if (p + 1 == memtet->l)
+					return (1);
+				else if (resolve_bit_aux(memtet, map, p + 1))
+					return (1);
+				toggle_tet(tet.mask, map, xi, yi);
+			}
+			xi++;
+		}
+		yi++;
+	}
+	return (0);
+}
+
+#include <stdio.h>
 
 void		resolve_bit(t_memtet_bit *memtet)
 {
-	/*
+	uint16_t map[16];
+
+	printf("Resolve_bit start\n");
+	ft_bzero(map, sizeof(uint16_t) * 16);
 	memtet->c = 2;
 	while (memtet->c * memtet->c < 4 * memtet->l)
 		memtet->c++;
-	while (!resolve_aux(memtet, 0))
+	while (!resolve_bit_aux(memtet, map, 0))
+	{
+		ft_bzero(map, sizeof(uint16_t) * 16);
 		memtet->c++;
-	*/
-	(void)memtet;
+	}
+	printf("Resolve_bit end\n");
 }
